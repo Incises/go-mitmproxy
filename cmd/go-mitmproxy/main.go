eeab22f9a636f10cc9e0b11aca3982e374b0e136
Zhengchao Ding <dingzc@bjtu.edu.cn>
2024-11-05 18:06:40 +0800

    add ms to logger timestamp

--- cmd/go-mitmproxy/main.go
+++ cmd/go-mitmproxy/main.go
@@ -48,7 +48,7 @@ func main() {
 	log.SetOutput(os.Stdout)
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp:   true,
-		TimestampFormat: "2006-01-02 15:04:05",
+		TimestampFormat: "2006-01-02 15:04:05.000",
 	})
 
 	opts := &proxy.Options{
